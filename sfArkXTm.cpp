4ef545e2b6bc9dad042ecd53dbaa1240c387c871
Arnout <arnouten@bzzt.net>
2013-02-02 19:04:59 +0100

    GPL header

--- sfArkXTm.cpp
+++ sfArkXTm.cpp
@@ -1,4 +1,22 @@
-//	Basic sfArk decompressor for Mac
+// Basic sfArk decompressor for Mac
+// Copyright 2002 Andy Inman
+// Contact via: http://netgenius.co.uk or http://melodymachine.com
+
+// This file is part of sfArkLib.
+//
+// sfArkLib is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// sfArkLib is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with sfArkLib.  If not, see <http://www.gnu.org/licenses/>.
+
 //  based on SDL_Test
 
 //  Reads an existing .sfArk file and writes an sf2 file using standard file i/o
